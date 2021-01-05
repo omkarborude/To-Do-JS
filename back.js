9508f7ffceef894a086e9b7d02af3af011d1c5b2
omkar <borudeomkar41@gmail.com>
2021-01-05 17:40:26 +0530

    dawd

--- back.js
+++ back.js
@@ -1,3 +1,19 @@
 var getin = document.getElementById('getin');
 var BTN = document.getElementById('BTN');
-var outuncompleted = document.getElementsByClassName('uncompletedTaskList');
+var uncompleted = document.getElementsByClassName('uncompletedTaskList');
+var completed = document.getElementsByClassName('completeTaskList');
+
+
+function addlist(event) {
+
+    var newli = document.createElement('li');
+    var doneBTN = document.createElement('button');
+    var delBTN = document.createElement('button');
+
+    doneBTN.innerHTML = '<i class="fa fa-check"></i>';
+    delBTN.innerHTML =  '<i class="fa fa-trash"></i>';
+
+    if(getin.value !== '') {
+        newli.innerText = getin.value;
+    }
+}
