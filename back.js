2c51b977c3f5c610456a77ba648ca5bacbf46e78
omkar <borudeomkar41@gmail.com>
2021-01-09 18:16:57 +0530

    bj

--- back.js
+++ back.js
@@ -1,10 +1,18 @@
 var getin = document.getElementById('getin');
 var BTN = document.getElementById('BTN');
-var uncompleted = document.getElementsByClassName('uncompletedTaskList');
-var completed = document.getElementsByClassName('completeTaskList');
+var uncompleted = document.querySelector('.uncompletedTaskList');
+var completed = document.querySelector('.completeTaskList');
+
+BTN.addEventListener('click', addlist);
+getin.addEventListener('keyup', (event)=> {
+    (event.keyCode === 13 ? addlist(event) : null);
+})
 
 
 function addlist(event) {
+
+    var uncompleted = document.querySelector('.uncompletedTaskList');
+    var completed = document.querySelector('.completeTaskList');
 
     var newli = document.createElement('li');
     var doneBTN = document.createElement('button');
@@ -13,8 +21,24 @@ function addlist(event) {
     doneBTN.innerHTML = '<i class="fa fa-check"></i>';
     delBTN.innerHTML =  '<i class="fa fa-trash"></i>';
 
-    if(getin.value !== '') {
-        newli.innerText = getin.value;
-        
+    
+    if(getin.value !=='') {
+        newli.textContent = getin.value;
+        getin.value = '';
+        uncompleted.appendChild(newli);
+        newli.appendChild(doneBTN);
+        newli.appendChild(delBTN);
     }
+
+    doneBTN.addEventListener('click', function() {
+        var parent = this.parentNode;
+        parent.remove();
+        completed.appendChild(parent);
+        doneBTN.style.display = 'none';
+    });
+
+    delBTN.addEventListener('click', function(){
+        var parent = this.parentNode;
+        parent.remove();
+    })
 }
